cb26d20b878dd028565d369f25c30ea9c8066b21
Eugene <evgeniy0ctreles@gmail.com>
2020-11-16 20:23:55 +0700

    Добавлена страница авторизации

---
+++ src/views/SignUp/SignUp.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-card
+    class="card mx-auto"
+  >
+    <v-form
+      ref="form"
+      v-model="form"
+      class="form pa-4"
+    >
+      <v-text-field
+        v-model="login"
+        :rules="[rules.required]"
+        label="Логин"
+        type="text"
+        color="light-green darken-2"
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        :rules="[rules.required, rules.length(6)]"
+        label="Пароль"
+        type="password"
+        color="light-green darken-2"
+      ></v-text-field>
+      <span>Забыли пароль? <router-link to="/forgot-password">Страница восстановления.</router-link></span>
+    </v-form>
+    <v-divider></v-divider>
+    <v-card-actions >
+      <v-btn
+        block
+        :disabled="!form"
+        :loading="isLoading"
+        class="white--text"
+        color="light-green darken-2"
+        
+      >
+        Войти
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      form: false,
+      login: undefined,
+      password: undefined,
+      rules: {
+        length: len => v => (v || '').length >= len || `Минимальная длина ${len}`,
+        required: v => !!v || 'Это обязательное поле',
+      },
+    }),
+  }
+</script>
+
+<style>
+  .card {
+    margin-bottom: auto;
+    margin-top: auto;
+  }
+  .form {
+    width: 500px;
+    
+  }
+</style>

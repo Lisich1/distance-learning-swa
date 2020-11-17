34bdf7a6dba6e2382fe872a30d7349765731b0e2
QumiQumi <glebyan1998@gmail.com>
2020-11-17 13:28:56 +0700

    add links to App

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
 	<div id="app">
 		<div id="nav">
 			<router-link to="/">Home</router-link> |
-			<router-link to="/forgot-password">Забыли пароль?</router-link> \
-			<router-link to="/reset-password">Восстановление пароля</router-link>
+			<router-link to="/forgot-password">Забыли пароль?</router-link> |
+			<router-link to="/reset-password">Восстановление пароля</router-link> |
+			<router-link to="/sign-up">Авторизация</router-link>
 		</div>
 		<router-view />
 	</div>

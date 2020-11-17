743776c2f2c981c374a61311b29d879b0724dfd4
Eugene <evgeniy0ctreles@gmail.com>
2020-11-17 14:20:40 +0700

    Вернул v-app для работы vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-	<div id="app">
+	<v-app id="app">
 		<div id="nav">
 			<router-link to="/">Home</router-link> |
 			<router-link to="/forgot-password">Забыли пароль?</router-link> \
 			<router-link to="/reset-password">Восстановление пароля</router-link>
 		</div>
 		<router-view />
-	</div>
+	</v-app>
 </template>
 
-<style>
-@import "./assets/Style.scss";
-</style>
 
 <style lang="scss">
+@import url("./assets/Style.scss");
 @import url("./views/Auth/auth.scss");
 </style>

5722fdbc75d889299ee92543ad0cf602838ca059
QumiQumi <glebyan1998@gmail.com>
2020-11-23 15:31:43 +0700

    make auth layout

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,6 +1,13 @@
 <template>
-	<div class="d-flex align-center justify-center auth-layout">
-		<router-view />
+	<div class="auth-layout">
+		<header class="auth-layout__logo d-flex justify-end align-center">
+			<div>logo</div>
+		</header>
+		<div class="auth-layout__image"></div>
+		<div class="auth-layout__content d-flex justify-end align-center">
+			<router-view />
+		</div>
+		<footer class="auth-layout__footer"></footer>
 	</div>
 </template>
 
@@ -12,6 +19,30 @@ export default {
 
 <style lang="scss">
 .auth-layout {
+	display: grid;
+	grid-template-rows: 1fr 1fr 1fr;
+	grid-template-columns: 1fr 1fr;
+	grid-template-areas:
+		"logo logo"
+		"image content"
+		"footer footer";
 	height: 100vh;
+
+	&__logo {
+		grid-area: logo;
+	}
+	&__image {
+		grid-area: image;
+		width: 558px;
+		height: 693px;
+
+		background: #7b92e3;
+	}
+	&__content {
+		grid-area: content;
+	}
+	&__footer {
+		grid-area: footer;
+	}
 }
 </style>
